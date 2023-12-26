13c97a40cdbedce5cbb41e3a1ce66a9f67ce52f1
Murat Tunç <murat.tunc8558@gmail.com>
2023-12-26 11:31:58 +0300

    Update Development_Ready.sh

--- Development_Ready.sh
+++ Development_Ready.sh
@@ -58,8 +58,3 @@ source ~/.profile
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 apt-get install postgresql
-
-go version
-
-
-
