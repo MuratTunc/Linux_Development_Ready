5a4feb21dd911b1382909214334f5630076ecce9
Murat Tunc <murat.tunc@siemens.com>
2023-12-27 15:16:46 +0300

    Addded color.

--- Development_Ready.sh
+++ Development_Ready.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 set -xe
+
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Clear the color after that
+clear='\033[0m'
+
 mail="xxx@mail.com"
 git_user_name="xxx"
 ##----------------------update and upgrade---------------------------------------##
