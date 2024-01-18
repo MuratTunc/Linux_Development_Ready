073cdb9adc4a4ba20e71fc1ab43deb58be1783e3
Murat Tunc <murat.tunc@siemens.com>
2024-01-18 12:02:02 +0300

    Added yes option for postgres installation

--- Development_Ready.sh
+++ Development_Ready.sh
@@ -20,18 +20,17 @@ git_user_name="xxx"
 #update-upgrade.
 echo -e "${blue}-->Status:Updating and Upgraiding system... ${clear}!"
 apt update -y
-sleep $slp
-apt-get upgrade -y
-echo -e "${blue}-->Status:Install curl... {clear}!"
-sleep $slp
+sleep ${slp}
+echo -e "${blue}-->Status:Install curl... ${clear}!"
+sleep ${slp}
 apt install curl -y
-echo -e "${blue}-->Status:essential... {clear}!"
-sleep $slp
+echo -e "${blue}-->Status:essential... ${clear}!"
+sleep ${slp}
 apt install build-essential -y
 
 ##-------------------------------------------------------------------------------##
 echo -e "${blue}-->Status:install git... ${clear}!"
-sleep $slp
+sleep ${slp}
 apt install git -y
 git config --global user.name "${git_user_name}"
 git config --global user.email "${mail}"
@@ -54,46 +53,46 @@ ssh-add ~/.ssh/id_rsa
 git_api_addkey="https://api.$(echo ${gitrepo_https} |cut -d'/' -f3)/user/keys"
 git_ssl_keyname="$(hostname)_$(date +%d-%m-%Y)"
 
-echo -e "${blue}-->Status:post ssh key... ${blue}blue${clear}!"
+echo -e "${blue}-->Status:post ssh key... ${blue}${clear}!"
 curl -H "Authorization: token ${git_api_token}" -H "Content-Type: application/json" -X POST -d "{\"title\":\"${git_ssl_keyname}\",\"key\":\"${sslpub}\"}" ${git_api_addkey}
 
 ##-------------------------------------------------------------------------------##
-echo -e "${blue}-->Status:installing golang... ${blue}blue${clear}!"
-sleep $slp
+echo -e "${blue}-->Status:installing golang... ${clear}!"
+sleep ${slp}
 go_version="go1.21.5.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 cd Downloads/
-echo -e "${blue}-->Status:Downloading go version ${go_version}... ${blue}blue${clear}!"
+echo -e "${blue}-->Status:Downloading go version ${go_version}... ${clear}!"
 wget https://dl.google.com/go/${go_version}
 sudo mv go1.21.5.linux-amd64.tar.gz /usr/local/
 cd /usr/local/
-echo -e "${blue}-->Status:untar downloaded file... ${go_version}... ${blue}blue${clear}!"
-sleep $slp
+echo -e "${blue}-->Status:untar downloaded file... ${go_version}... ${clear}!"
+sleep ${slp}
 sudo tar -C /usr/local/ -xzf ${go_version}
 
 ##-------------------------------------------------------------------------------##
 #Add the path /usr/local/go/bin to the $PATH environment variable.
-echo -e "${blue}-->Status:EXORT PATH variable to profile... ${go_version}... ${blue}blue${clear}!"
-sleep $slp
+echo -e "${blue}-->Status:EXORT PATH variable to profile... ${go_version}... ${clear}!"
+sleep ${slp}
 echo -e "\n# path added by my personal installer" >> ~/.profile
 echo "[ -d /usr/local/go/bin ] && PATH=\"/usr/local/go/bin:\$PATH\"" >> ~/.profile
 source ~/.profile
 
 ##-------------------------------------------------------------------------------##
 #Add the path /usr/local/go/bin to the $PATH environment variable.
-echo -e "${blue}-->Status:EXORT PATH variable to bashrc... ${go_version}... ${blue}blue${clear}!"
-sleep $slp
+echo -e "${blue}-->Status:EXORT PATH variable to bashrc... ${go_version}... ${clear}!"
+sleep ${slp}
 echo -e "\n# path added by my personal installer" >> ~/.profile
 echo "[ -d /usr/local/go/bin ] && PATH=\"/usr/local/go/bin:\$PATH\"" >> ~/.bashrc
 source ~/.bashrc
 
 ##-------------------------------------------------------------------------------##
 #install postgreSQL
-echo -e "${blue}-->Status:installing postgres... ${blue}blue${clear}!"
-sleep $slp
+echo -e "${blue}-->Status:installing postgres... ${clear}!"
+sleep ${slp}
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt-get install postgresql
+apt-get install postgresql -y
 
 ##-------------------------------------------------------------------------------##
 echo -e "${green}-->Status:Process ENDED !... ${clear}!"
