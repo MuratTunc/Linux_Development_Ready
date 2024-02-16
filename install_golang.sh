937597c62f444888eacffa451556197e3a012d72
Murat Tunc <murat.tunc@siemens.com>
2024-02-16 15:27:50 +0300

    Adjust path variable!

--- install_golang.sh
+++ install_golang.sh
@@ -14,32 +14,42 @@ clear='\033[0m'
 ##-------------------------------------------------------------------------------##
 # Clear the screen
 clear
-echo -e "${green}-->Status:Remove old files from /usr/local... ${clear}!"
-rm -rf /usr/local/go
-echo -e "${green}-->Status:Downloading go version ${go_version}... ${clear}!"
 
+echo -e "${green}-->Status:Getting latest go version tar file from web source...${clear}!"
 http_response=$(GET https://go.dev/dl/)
 extracted_response=$(echo "$http_response" | grep -o -P '(?<=class="download downloadBox" href=).*?(?=>)')
 go_version_full=$(echo "$extracted_response" | grep "linux" )
 len=${#go_version_full}
 go_version=${go_version_full:5:len-6}
-wget https://dl.google.com/go/${go_version}
+echo -e "${green}-->Status:Latest golang linux tar file: ${go_version} ...${clear}!"
 
+##-------------------------------------------------------------------------------##
+echo -e "${green}-->Status:Removing old go tar files from /usr/local... ${clear}!"
+rm -rf /usr/local/go
+rm -rf /usr/local/go_version
+rm -rf go_version
+sleep ${slp}
+echo -e "${green}-->Status:Downloading go version ${go_version}... ${clear}!"
+wget https://dl.google.com/go/${go_version}
+##-------------------------------------------------------------------------------##
+
+##-------------------------------------------------------------------------------##
 sleep ${slp}
 mv ${go_version} /usr/local/
 cd /usr/local/
-echo -e "${green}-->Status:untar downloaded file... ${go_version}... ${clear}!"
+echo -e "${green}-->Status:Untar downloaded ${go_version} file... ${clear}!"
 sleep ${slp}
 tar -C /usr/local/ -xzf ${go_version}
 sleep ${slp}
+##-------------------------------------------------------------------------------##
 
+##-------------------------------------------------------------------------------##
 #Add the path /usr/local/go/bin to the $PATH environment variable.(profile)
-
-echo -e "${blue}-->Status:EXPORT PATH variable to profile... ${go_version}... ${clear}!"
+echo -e "${green}-->Status:EXPORT PATH variable to profile... ${go_version}... ${clear}!"
 sleep ${slp}
-echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
 sleep ${slp}
-source ~/.bashrc
+source ~/.profile
 sleep ${slp}
-echo -e "${green}-->go version... ${clear}!"
-go version
+echo -e "${green}-->go version...$(go version) ${clear}!"
+##-------------------------------------------------------------------------------##
---
+++ myscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -e "${blue}-->Status:EXPORT PATH variable to profile... ${go_version}... ${clear}!"
+sleep 5
+echo 'PATH=$PATH:/usr/local/go/bin' >> filedummy.txt
